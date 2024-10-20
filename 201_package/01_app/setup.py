6f64126d42249bfab8e7274fe0af8c5acbf96ed6
crayoncrayon2003 <crayon.crayon2003@gmail.com>
2024-10-21 00:52:36 +0900

    main

--- 201_package/01_app/setup.py
+++ 201_package/01_app/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='myapp',                       # パッケージ名（pip listで表示される）
-    version="0.0.1",                    # バージョン
-    description="this is description",  # 説明
-    author='haneya',                    # 作者名
-    packages=find_packages(),           # 使うモジュール一覧を指定する
-    license='MIT'                       # ライセンス
+    name='myapp',                       # packages name. this is displayed in "pip list"
+    version="0.0.1",                    # packages version
+    description="this is description",  # packages description
+    author='anonymous',                 # packages author
+    packages=find_packages(),           # the list of modules to use
+    license='MIT'                       # packages license
 )
