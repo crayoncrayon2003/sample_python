58b8b2cc2733117c77d94d7044dcab0d6d283a88
crayoncrayon2003 <crayon.crayon2003@gmail.com>
2025-01-21 17:12:42 +0900

    main

--- 021_Keyring/01_set_password.py
+++ 021_Keyring/01_set_password.py
@@ -1,7 +1,11 @@
 import keyring
+import getpass
 
 service_id = 'IM_YOUR_APP!'
 username = 'user'
 password = 'password'
+
+# username = input("input username : ")
+# password = getpass.getpass("input password : ")
 
 keyring.set_password(service_id, username, password)
